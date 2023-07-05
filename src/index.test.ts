9b9a10828018fbf24a3272970c5b9d0e3de56653
ootidea <ootidea@gmail.com>
2023-07-06 07:32:12 +0900

    env: prettier "semi": false

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,19 +1,19 @@
-import { describe, expect, test } from 'vitest';
-import { operatorsOf, TAG_KEY, TaggedUnion } from './index';
+import { describe, expect, test } from 'vitest'
+import { operatorsOf, TAG_KEY, TaggedUnion } from './index'
 
 describe('operatorsOf', () => {
-  type Shape = TaggedUnion<{ Rect: { width: number; height: number }; Circle: { radius: number } }>;
-  const Shape = operatorsOf<Shape>();
-  const circle = Shape.Circle({ radius: 3 }) as Shape;
+  type Shape = TaggedUnion<{ Rect: { width: number; height: number }; Circle: { radius: number } }>
+  const Shape = operatorsOf<Shape>()
+  const circle = Shape.Circle({ radius: 3 }) as Shape
 
   test('Data constructors', () => {
-    expect(Shape.Circle({ radius: 3 })).toStrictEqual({ [TAG_KEY]: 'Circle', radius: 3 });
-  });
+    expect(Shape.Circle({ radius: 3 })).toStrictEqual({ [TAG_KEY]: 'Circle', radius: 3 })
+  })
 
   test('Type predicates', () => {
-    expect(Shape.is.Circle(circle)).toStrictEqual(true);
-    expect(Shape.is.Rect(circle)).toStrictEqual(false);
-  });
+    expect(Shape.is.Circle(circle)).toStrictEqual(true)
+    expect(Shape.is.Rect(circle)).toStrictEqual(false)
+  })
 
   test('match', () => {
     expect(
@@ -21,6 +21,6 @@ describe('operatorsOf', () => {
         Rect: ({ width, height }) => width * height,
         Circle: ({ radius }) => radius * radius * Math.PI,
       }),
-    ).toBe(3 * 3 * Math.PI);
-  });
-});
+    ).toBe(3 * 3 * Math.PI)
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,64 @@
-export const TAG_KEY = Symbol();
+export const TAG_KEY = Symbol()
 
 export type TaggedUnion<T extends Record<string, any>> = {
-  [K in keyof T]: Simplify<{ [TAG_KEY]: K } & T[K]>;
-}[keyof T];
+  [K in keyof T]: Simplify<{ [TAG_KEY]: K } & T[K]>
+}[keyof T]
 
-type Simplify<T> = T extends T ? { [K in keyof T]: T[K] } : never;
+type Simplify<T> = T extends T ? { [K in keyof T]: T[K] } : never
 
 type Payload<Adt extends { [TAG_KEY]: string }, K extends Adt[typeof TAG_KEY]> = Simplify<
   Omit<Simplify<Adt & { [TAG_KEY]: K }>, typeof TAG_KEY>
->;
+>
 
 type Is<Adt extends { [TAG_KEY]: string }> = Simplify<{
-  [K in Adt[typeof TAG_KEY]]: (adt: Adt) => adt is Simplify<Adt & { [TAG_KEY]: K }>;
-}>;
+  [K in Adt[typeof TAG_KEY]]: (adt: Adt) => adt is Simplify<Adt & { [TAG_KEY]: K }>
+}>
 function createIs<Adt extends { [TAG_KEY]: string }>(): Is<Adt> {
   return new Proxy(
     {},
     {
       get(target, key) {
-        return (adt: Adt) => adt[TAG_KEY] === key;
+        return (adt: Adt) => adt[TAG_KEY] === key
       },
     },
-  ) as any;
+  ) as any
 }
 
 export function operatorsOf<Adt extends { [TAG_KEY]: string }>(): Simplify<
   {
     match: <
       Matchers extends {
-        [K in Adt[typeof TAG_KEY]]: (payload: Payload<Adt, K>) => unknown;
+        [K in Adt[typeof TAG_KEY]]: (payload: Payload<Adt, K>) => unknown
       },
     >(
       adt: Adt,
       matchers: Matchers,
-    ) => ReturnType<Matchers[keyof Matchers]>;
-    is: Is<Adt>;
+    ) => ReturnType<Matchers[keyof Matchers]>
+    is: Is<Adt>
   } & {
-    [K in Adt[typeof TAG_KEY]]: <const T extends Payload<Adt, K>>(payload: T) => Simplify<T & { [TAG_KEY]: K }>;
+    [K in Adt[typeof TAG_KEY]]: <const T extends Payload<Adt, K>>(payload: T) => Simplify<T & { [TAG_KEY]: K }>
   }
 > {
   return new Proxy(
     {
       match: <
         Matchers extends {
-          [K in Adt[typeof TAG_KEY]]: (payload: Payload<Adt, K>) => unknown;
+          [K in Adt[typeof TAG_KEY]]: (payload: Payload<Adt, K>) => unknown
         },
       >(
         adt: Adt,
         matchers: Matchers,
       ): ReturnType<Matchers[keyof Matchers]> => {
-        return (matchers as any)[adt[TAG_KEY]](adt);
+        return (matchers as any)[adt[TAG_KEY]](adt)
       },
       is: createIs<Adt>(),
     },
     {
       get(target, key) {
-        if (Reflect.has(target, key)) return Reflect.get(target, key);
+        if (Reflect.has(target, key)) return Reflect.get(target, key)
 
-        return (value: object) => ({ ...value, [TAG_KEY]: key });
+        return (value: object) => ({ ...value, [TAG_KEY]: key })
       },
     },
-  ) as any;
+  ) as any
 }
